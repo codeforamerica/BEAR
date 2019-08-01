3393e6bb38468eb8683c361d47b3fecb6a6aaa10
Molly T-M <mollyt@codeforamerica.org>
2019-08-01 13:32:21 -0700

    Run all integration tests
    - include clicking radio buttons and checkboxes
    
    [#167323119]

--- test/e2e/homeIntegration.spec.js
+++ test/e2e/homeIntegration.spec.js
@@ -119,24 +119,22 @@ describe('The happy path', () => {
     expect(pageTitle).toContain('Baseline eligibility');
   });
 
-  // // TODO Must fix ability to click a reduce option for this test to work
-  // it('can select eligibility options and display additional relief page', async () => {
-  //   await app.client.click('#begin');
-  //
-  //   const countySelect = app.client.$('#county-select');
-  //   await countySelect.selectByVisibleText('Sacramento');
-  //   await app.client.click('#continue');
-  //
-  //   await app.client.chooseFile('#doj-file-input', './test/fixtures/file.dat');
-  //   await app.client.click('#continue');
-  //
-  //   // Clicking the checkbox does not currently work: Error: unknown error: Element <input type="radio" class="RadioButton__radioButton__11Izr" name="11360" id="reduce_11360"> is not clickable at point (850, 695). Other element would receive the click: <html>...</html>
-  //   // await app.client.click('#reduce_11358');
-  //   await app.client.click('#continue');
-  //
-  //   const cardContent = await app.client.getText('.form-card__title');
-  //   expect(cardContent).toContain('Additional relief');
-  // });
+  it('can select eligibility options and display additional relief page', async () => {
+    await app.client.click('#begin');
+
+    const countySelect = app.client.$('#county-select');
+    await countySelect.selectByVisibleText('Sacramento');
+    await app.client.click('#continue');
+
+    await app.client.chooseFile('#doj-file-input', './test/fixtures/file.dat');
+    await app.client.click('#continue');
+
+    await app.client.click('#reduce_11358');
+    await app.client.click('#continue');
+
+    const cardContent = await app.client.getText('.form-card__title');
+    expect(cardContent).toContain('Additional relief');
+  });
 
   it('can skip additional relief page if all eligibility options are dismiss', async () => {
     await app.client.click('#begin');
@@ -159,45 +157,36 @@ describe('The happy path', () => {
     );
   });
 
-  // TODO Must fix ability to click a reduce option for this test to work
-  // it('can select additional relief options and display results page', async () => {
-  //   // jest.setTimeout(30000);
-  //   await app.client.click('#begin');
-  //
-  //   const countySelect = app.client.$('#county-select');
-  //   await countySelect.selectByVisibleText('Sacramento');
-  //   await app.client.click('#continue');
-  //
-  //   await app.client.chooseFile('#doj-file-input', './test/fixtures/file.dat');
-  //   await app.client.click('#continue');
-  //
-  //   // Clicking the checkbox does not currently work: Error: unknown error: Element <input type="radio" class="RadioButton__radioButton__11Izr" name="11360" id="reduce_11360"> is not clickable at point (850, 695). Other element would receive the click: <html>...</html>
-  //   // await app.client.click('#reduce_11360');
-  //   await app.client.click('#continue');
-  //
-  //   await app.client.click('#true_subjectUnder21AtConviction');
-  //   const ageSelect = app.client.$('#subjectAgeThreshold-select');
-  //   await ageSelect.selectByVisibleText('45');
-  //
-  //   const yearSelect = app.client.$('#yearsSinceConvictionThreshold-select');
-  //   await yearSelect.selectByVisibleText('3');
-  //
-  //   await app.client.click('#true_subjectHasOnlyProp64Charges');
-  //   await app.client.click('#continue');
-  //
-  //   const processingCardContent = await app.client.getText(
-  //     '.form-card__content h3'
-  //   );
-  //   expect(processingCardContent).toContain(
-  //     'Reading and preparing your files ...'
-  //   );
-  //
-  //   await sleep(11);
-  //   const resultsFormCardContent = await app.client.getText(
-  //     '.form-card__title'
-  //   );
-  //   expect(resultsFormCardContent).toContain('Your files are ready!');
-  // });
+  it('can select additional relief options and display processing page', async () => {
+    await app.client.click('#begin');
+
+    const countySelect = app.client.$('#county-select');
+    await countySelect.selectByVisibleText('Sacramento');
+    await app.client.click('#continue');
+
+    await app.client.chooseFile('#doj-file-input', './test/fixtures/file.dat');
+    await app.client.click('#continue');
+
+    await app.client.click('#reduce_11360');
+    await app.client.click('#continue');
+
+    await app.client.click('#true_subjectUnder21AtConviction');
+    const ageSelect = app.client.$('#subjectAgeThreshold-select');
+    await ageSelect.selectByVisibleText('45');
+
+    const yearSelect = app.client.$('#yearsSinceConvictionThreshold-select');
+    await yearSelect.selectByVisibleText('3');
+
+    await app.client.click('#true_subjectHasOnlyProp64Charges');
+    await app.client.click('#continue');
+
+    const processingCardContent = await app.client.getText(
+      '.form-card__content h3'
+    );
+    expect(processingCardContent).toContain(
+      'Reading and preparing your files ...'
+    );
+  });
 
   it('can generate correct eligibility config file if all eligibility options are dismiss', async () => {
     await app.client.click('#begin');
@@ -248,65 +237,64 @@ describe('The happy path', () => {
     });
   });
 
-  // TODO Must fix ability to click a reduce option for this test to work
-  // it('can generate correct eligibility config file if some code sections are reduced and additional relief is chosen', async () => {
-  //   await app.client.click('#begin');
-  //
-  //   const countySelect = app.client.$('#county-select');
-  //   await countySelect.selectByVisibleText('Sacramento');
-  //   await app.client.click('#continue');
-  //
-  //   await app.client.chooseFile('#doj-file-input', './test/fixtures/file.dat');
-  //   await app.client.click('#continue');
-  //
-  //   await app.client.click('#reduce_11360');
-  //   await app.client.click('#continue');
-  //
-  //   await app.client.click('#true_subjectUnder21AtConviction');
-  //   const ageSelect = app.client.$('#subjectAgeThreshold-select');
-  //   await ageSelect.selectByVisibleText('45');
-  //
-  //   const yearSelect = app.client.$('#yearsSinceConvictionThreshold-select');
-  //   await yearSelect.selectByVisibleText('3');
-  //
-  //   await app.client.click('#true_subjectHasOnlyProp64Charges');
-  //   await app.client.click('#continue');
-  //
-  //   outputDirectory = getOutputDirectoryPath(getDateTime());
-  //   const eligibilityConfigFilePath = `${outputDirectory}/eligibilityConfig_${getDateTime()}.json`;
-  //
-  //   const eligibilityConfigFileContents = fs.readFileSync(
-  //     eligibilityConfigFilePath,
-  //     'utf8'
-  //   );
-  //   const eligibilityConfig = JSON.parse(eligibilityConfigFileContents);
-  //
-  //   expect(eligibilityConfig).toEqual({
-  //     baselineEligibility: {
-  //       dismiss: [
-  //         '11357(a)',
-  //         '11357(b)',
-  //         '11357(c)',
-  //         '11357(d)',
-  //         '11357(no-sub-section)',
-  //         '11358',
-  //         '11359'
-  //       ],
-  //       reduce: ['11360']
-  //     },
-  //     additionalRelief: {
-  //       subjectUnder21AtConviction: false,
-  //       dismissOlderThanAgeThreshold: true,
-  //       subjectAgeThreshold: 45,
-  //       dismissYearsSinceConvictionThreshold: true,
-  //       yearsSinceConvictionThreshold: 3,
-  //       dismissYearsCrimeFreeThreshold: true,
-  //       yearsCrimeFreeThreshold: 5,
-  //       subjectHasOnlyProp64Charges: false,
-  //       subjectIsDeceased: true
-  //     }
-  //   });
-  // });
+  it('can generate correct eligibility config file if some code sections are reduced and additional relief is chosen', async () => {
+    await app.client.click('#begin');
+
+    const countySelect = app.client.$('#county-select');
+    await countySelect.selectByVisibleText('Sacramento');
+    await app.client.click('#continue');
+
+    await app.client.chooseFile('#doj-file-input', './test/fixtures/file.dat');
+    await app.client.click('#continue');
+
+    await app.client.click('#reduce_11360');
+    await app.client.click('#continue');
+
+    await app.client.click('#true_subjectUnder21AtConviction');
+    const ageSelect = app.client.$('#subjectAgeThreshold-select');
+    await ageSelect.selectByVisibleText('45');
+
+    const yearSelect = app.client.$('#yearsSinceConvictionThreshold-select');
+    await yearSelect.selectByVisibleText('3');
+
+    await app.client.click('#true_subjectHasOnlyProp64Charges');
+    await app.client.click('#continue');
+
+    outputDirectory = getOutputDirectoryPath(getDateTime());
+    const eligibilityConfigFilePath = `${outputDirectory}/eligibilityConfig_${getDateTime()}.json`;
+
+    const eligibilityConfigFileContents = fs.readFileSync(
+      eligibilityConfigFilePath,
+      'utf8'
+    );
+    const eligibilityConfig = JSON.parse(eligibilityConfigFileContents);
+
+    expect(eligibilityConfig).toEqual({
+      baselineEligibility: {
+        dismiss: [
+          '11357(a)',
+          '11357(b)',
+          '11357(c)',
+          '11357(d)',
+          '11357(no-sub-section)',
+          '11358',
+          '11359'
+        ],
+        reduce: ['11360']
+      },
+      additionalRelief: {
+        subjectUnder21AtConviction: false,
+        dismissOlderThanAgeThreshold: true,
+        subjectAgeThreshold: 45,
+        dismissYearsSinceConvictionThreshold: true,
+        yearsSinceConvictionThreshold: 3,
+        dismissYearsCrimeFreeThreshold: true,
+        yearsCrimeFreeThreshold: 5,
+        subjectHasOnlyProp64Charges: false,
+        subjectIsDeceased: true
+      }
+    });
+  });
 
   it('can complete process and display results page', async () => {
     jest.setTimeout(30000);
