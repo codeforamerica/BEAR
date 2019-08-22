de7ab43d6c205507a6e939a7d6c0829c872e0ab8
Vraj Mohan <vraj@codeforamerica.org>
2019-08-22 13:44:22 -0700

    Moved functions into ErrorFormCard component
    [#167308382]
    
    Co-authored-by: Molly TM <mollyt@codeforamerica.org>

--- app/components/ErrorFormCard.js
+++ app/components/ErrorFormCard.js
@@ -14,54 +14,50 @@ type Props = {
   errorText: string
 };
 
-type ErrorSectionProps = {
-  title: string,
-  errors: Object
-};
-
 const preStyle = {
   outline: 'auto thin',
   textAlign: 'left'
 };
 
-function extractErrors(errorData: Object, fileparsing: boolean) {
-  let keys;
-  if (fileparsing) {
-    keys = Object.keys(errorData).filter(key => errorData[key].ExitCode === 2);
-  } else {
-    keys = Object.keys(errorData).filter(key => errorData[key].ExitCode !== 2);
-  }
-  return keys.reduce(
-    (res, key) => Object.assign(res, { [key]: errorData[key] }),
-    {}
-  );
-}
-
-/**
- * @return {null}
- */
-function ErrorSection(props: ErrorSectionProps) {
-  const { title, errors } = props;
-
-  if (Object.keys(errors).length !== 0) {
-    return (
-      <div>
-        <h4> {title} </h4>
-        <pre style={preStyle}>
-          {Object.keys(errors).map(item => (
-            <p key={item}>{`${item}: ${errors[item].ErrorMessage}`}</p>
-          ))}
-        </pre>
-      </div>
-    );
-  }
-  return null;
-}
-
 export default class ErrorFormCard extends Component<Props> {
   componentDidMount() {
     window.scrollTo(0, 0);
   }
+
+  extractErrors = (errorData: Object, fileparsing: boolean) => {
+    let keys;
+    if (fileparsing) {
+      keys = Object.keys(errorData).filter(
+        key => errorData[key].ExitCode === 2
+      );
+    } else {
+      keys = Object.keys(errorData).filter(
+        key => errorData[key].ExitCode !== 2
+      );
+    }
+    return keys.reduce(
+      (res, key) => Object.assign(res, { [key]: errorData[key] }),
+      {}
+    );
+  };
+
+  renderErrorSection = (errorData: Object, parsing: boolean, title: string) => {
+    const errors = this.extractErrors(errorData, parsing);
+
+    if (Object.keys(errors).length !== 0) {
+      return (
+        <div>
+          <h4> {title} </h4>
+          <pre style={preStyle}>
+            {Object.keys(errors).map(item => (
+              <p key={item}>{`${item}: ${errors[item].ErrorMessage}`}</p>
+            ))}
+          </pre>
+        </div>
+      );
+    }
+    return null;
+  };
 
   onClickStartOver = () => {
     const { onStartOver } = this.props;
@@ -86,14 +82,8 @@ export default class ErrorFormCard extends Component<Props> {
                     Sorry, we had trouble with your request. Please try again.
                   </p>
                   <div>
-                    <ErrorSection
-                      title="Parsing"
-                      errors={extractErrors(errorData, true)}
-                    />
-                    <ErrorSection
-                      title="Non Parsing"
-                      errors={extractErrors(errorData, false)}
-                    />
+                    {this.renderErrorSection(errorData, true, 'Parsing')}
+                    {this.renderErrorSection(errorData, false, 'Non Parsing')}
                   </div>
                 </div>
               </div>
