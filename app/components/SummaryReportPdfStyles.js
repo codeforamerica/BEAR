86733b9b203b7fd22e623f21c6901e672c3819ca
Christa Hartsock <christa@codeforamerica.org>
2019-08-29 16:03:43 -0700

    Style updates to summary PDF
    * Removes fonts for now, since there are issues serving them properly in
    the built application.

--- app/components/SummaryReportPdfStyles.js
+++ app/components/SummaryReportPdfStyles.js
@@ -1,15 +1,33 @@
 import { StyleSheet } from '@react-pdf/renderer';
-
-// Font.register({
-//   family: 'Roboto',
-//   src: path.join(__dirname, '/assets/fonts/Roboto-Medium.ttf')
-// });
+// import robotoBold from '../assets/fonts/Roboto-Bold.ttf';
+// import robotoRegular from '../assets/fonts/Roboto-Regular.ttf';
+//
 // Font.register({
 //   family: 'Roboto-Bold',
-//   src: path.join(__dirname, '/assets/fonts/Roboto-Bold.ttf')
+//   src: robotoBold
+// });
+//
+// Font.register({
+//   family: 'Roboto',
+//   src: robotoRegular
 // });
 
+const baseTextStyles = {
+  fontSize: 12,
+  lineHeight: 1.5
+  // fontFamily: 'Roboto'
+};
+
+const baseHxStyles = {
+  color: '#008060',
+  fontWeight: 'bold'
+};
+
 const styles = StyleSheet.create({
+  page: {
+    paddingBottom: 20,
+    paddingTop: 40
+  },
   logoImage: {
     position: 'absolute',
     top: 0,
@@ -17,7 +35,9 @@ const styles = StyleSheet.create({
     width: 200
   },
   bulletImage: {
-    position: 'absolute'
+    position: 'absolute',
+    height: 6,
+    marginRight: 20
   },
   headerText: {
     width: 300,
@@ -33,53 +53,65 @@ const styles = StyleSheet.create({
   header: {
     borderBottom: 7,
     borderColor: '#008060',
-    marginVertical: 20,
+    marginTop: -20,
+    marginBottom: 20,
     position: 'relative',
     height: 100
   },
   body: {
     paddingTop: 15,
-    paddingBottom: 65,
-    paddingHorizontal: 35
+    paddingHorizontal: 35,
+    paddingRight: 65
   },
   textWithPadding: {
+    ...baseTextStyles,
     paddingTop: 8
   },
   h1: {
-    marginBottom: 8,
+    marginBottom: 10,
     fontSize: 24
     // fontFamily: 'Roboto-Bold'
   },
   h2: {
-    marginTop: 16,
+    ...baseHxStyles,
+    marginTop: 18,
     marginBottom: 12,
-    color: '#008060',
     fontSize: 14
-    // fontFamily: 'Roboto-Bold'
   },
   h3: {
-    marginVertical: 10,
-    color: '#008060',
+    ...baseHxStyles,
+    marginTop: 16,
+    marginBottom: 10,
     fontSize: 12
-    // fontFamily: 'Roboto-Bold'
+  },
+  h3Condensed: {
+    ...baseHxStyles,
+    fontSize: 12,
+    marginTop: 8,
+    marginBottom: 10
   },
   text: {
-    fontSize: 12
+    ...baseTextStyles
+  },
+  indentedText: {
+    ...baseTextStyles,
+    marginLeft: 16,
+    marginBottom: 10
   },
   boldText: {
-    fontSize: 12
+    ...baseTextStyles
     // fontFamily: 'Roboto-Bold'
   },
   listItem: {
-    fontSize: 12,
-    paddingVertical: 8,
+    ...baseTextStyles,
+    marginBottom: 8,
     paddingLeft: 15
   },
-  pageBreak: {
+  horizontalRule: {
     borderBottom: 1,
     borderColor: '#008060',
     marginHorizontal: 130,
-    marginTop: 8
+    marginTop: 12
   }
 });
 
