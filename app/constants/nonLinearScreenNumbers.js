6c12ed5235770cd2004b696c7590261b547861ba
Christa Hartsock <christa@codeforamerica.org>
2019-09-19 15:40:32 -0700

    Remove privacy policy from code base
    [Finishes #168149289]

--- app/constants/nonLinearScreenNumbers.js
+++ app/constants/nonLinearScreenNumbers.js
@@ -1,9 +1,8 @@
 // These numbers depend on the order in which the non-linear screens are rendered inside the page container in the home component
 const nonLinearScreenNumbers = {
-  privacyPolicy: 7,
-  faq: 8,
-  errorScreen: 9,
-  termsOfService: 10
+  faq: 7,
+  errorScreen: 8,
+  termsOfService: 9
 };
 
 export default nonLinearScreenNumbers;
